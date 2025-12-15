369a8796d6a15aa354837f7693949682a8ab3d57
redrix-dev <redrixdevelopment@icloud.com>
2025-12-15 01:01:29 -0500

    Use mjs tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
     "./app/**/*.{ts,tsx,mdx}",
     "./components/**/*.{ts,tsx}",
